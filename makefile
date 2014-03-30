f19faf5c724fa0d74545f436d99e6c2168ae3d11
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-03-30 17:21:09 -0400

    updating makefile + new file

--- makefile
+++ makefile
@@ -1,7 +1,10 @@
-all: main
+all: main	twitter_gender
 
 main:	main.cpp
 	g++ -lopencv_core -lopencv_highgui -lopencv_contrib main.cpp -o main
+
+twitter_gender:	twitter_gender.cpp
+	g++ -lopencv_core -lopencv_highgui -lopencv_contrib -lcurl twitter_gender.cpp -o twitter_gender
 
 clean:
 	rm -f main
---
+++ twitter_gender.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <curl/curl.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+    std::ifstream file(filename.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(CV_StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+};
+
+int main(int argc, const char *argv[]) {
+  string fn_csv = "model.csv";
+  string fn_predict = argv[1];
+
+  vector<Mat> modelImages;
+  vector<int> modelLabels;
+
+  read_csv( fn_csv, modelImages, modelLabels );
+
+  Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
+  model->train( modelImages, modelLabels );
+
+  std::cout << "Finding match for @" << fn_predict << std::endl;
+  Mat testImage =  imread(fn_predict, 0);
+
+  int predictedLabel =-1;
+  double predictedConfidence = 0.0;
+
+/*  model->predict(testImage, predictedLabel, predictedConfidence);
+
+  std::cout << "'" << fn_predict << "' => " << predictedLabel << std::endl;
+  std::cout << "Confidence: " << predictedConfidence << std::endl;*/
+};
