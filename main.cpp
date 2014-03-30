44c42060bf96b6fcc0a421af5007d9d1574aaa35
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2014-03-30 16:59:31 -0400

    main program

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace std;
+
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+    std::ifstream file(filename.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given filename.";
+        CV_Error(CV_StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            images.push_back(imread(path, 0));
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+};
+
+int main() {
+  string fn_csv = "model.csv";
+
+  vector<Mat> modelImages;
+  vector<int> modelLabels;
+
+  read_csv( fn_csv, modelImages, modelLabels );
+
+  Ptr<FaceRecognizer> model = createLBPHFaceRecognizer();
+  model->train( modelImages, modelLabels );
+};
